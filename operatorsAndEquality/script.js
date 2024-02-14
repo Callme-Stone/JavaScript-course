0acdcc74eb974dd1715479d305cf781d5c7f2a1f
Callme-Stone <abolajioluwaseyi0@gmail.com>
2024-02-14 09:41:36 +0100

    first commit

---
+++ operatorsAndEquality/script.js
@@ -0,0 +1,25 @@
+const a = 5;
+const b = 30;
+let result = 0;
+
+result = a + b;
+console.log(result);
+
+result = a * b;
+console.log(result);
+
+result = a / b;
+console.log(result);
+
+result = a - b;
+console.log(result);
+
+result = a % b;
+console.log(result);
+
+result = a ** b;
+console.log(result);
+
+let number = 5
+result = ++number
+console.log(result);
---
+++ variable/script.js
@@ -0,0 +1,98 @@
+// Variables.
+
+// 
+
+let message = "Hello Eef";
+message = 'Hello Seyi'
+message = 'Az'
+console.log(message);
+
+const greeting = "Good morning my guy";
+console.log(greeting)
+
+const _name$ = "YO"
+console.log(_name$)
+
+console.log(_name$, message, greeting)
+
+// Create a variable, call it Student Name with a value that you like. Make sure the value can be reassigned in the future. Let us see your result on your console
+// Create 3 variables, the naming must be a mixture of the $ sign, number ansd underscore respectively 
+
+let studentName = "Hello Student Seyi"
+studentName = "Student seyi come here"
+console.log(studentName)
+
+const _name = "Bawo ni"
+const greetings5 = "Hope you slept well?"
+const meeting$ = "Come meet me with the cash"
+
+console.log(_name, greetings5, meeting$)
+
+// Stings
+// Single string - ''
+// Double String - ""
+// Back ticks - ``
+
+const singleQuote = 'Who am I?';
+const doubleQuote = "Who are you?";
+
+console.log(singleQuote);
+console.log(doubleQuote);
+
+let name = "Team Lead";
+const backTicks = `Hello ${name}`;
+console.log(backTicks);
+
+let newName = 'Olakanmi';
+let favouriteDrink = 'Pepsi';
+
+const details = `My name is ${newName}, and my favourite drink is ${favouriteDrink}`;
+console.log(details)
+
+const numberOne = 45;
+const numberTwo = 10;
+
+const add = numberOne + numberTwo;
+console.log(add);
+
+const sub = numberOne - numberTwo;
+console.log(sub);
+
+const div = numberOne / numberTwo;
+console.log(div);
+
+const mul = `${numberOne * numberTwo}`;
+console.log(mul);
+
+const numberThree = '10';
+const numberFour = '14';
+console.log(numberThree + numberFour)
+
+console.log('10 + 15');
+console.log('10 - 15');
+
+//Boolean - true / false
+
+const isBeautiful = false;
+// console.log(typeof isBeautiful)
+
+if (!isBeautiful) {
+    console.log("This woman is beautiful")
+} else {
+    console.log("This woman is  ugly")
+}
+
+//Null
+
+let age = null;
+console.log(age);
+
+//Undefined
+let x;
+console.log(x);
+
+// Symbol
+const value1 = Symbol("Hello");
+const value2 = Symbol("Hello");
+console.log(value1.description);
+
