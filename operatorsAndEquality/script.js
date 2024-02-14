ce00739d79066c3ff803d41a4ef1c9524c4783df
Callme-Stone <abolajioluwaseyi0@gmail.com>
2024-02-15 00:17:14 +0100

    logicAndControlFlow

--- operatorsAndEquality/script.js
+++ operatorsAndEquality/script.js
@@ -1,25 +1,93 @@
-const a = 5;
-const b = 30;
-let result = 0;
+// Arithmetic Operation
+// const a = 5;
+// const b = 30;
+// let result = 0;
 
-result = a + b;
-console.log(result);
+// result = a + b;
+// console.log(result);
 
-result = a * b;
-console.log(result);
+// result = a * b;
+// console.log(result);
 
-result = a / b;
-console.log(result);
+// result = a / b;
+// console.log(result);
 
-result = a - b;
-console.log(result);
+// result = a - b;
+// console.log(result);
 
-result = a % b;
-console.log(result);
+// result = a % b;
+// console.log(result);
 
-result = a ** b;
-console.log(result);
+// result = a ** b;
+// console.log(result);
+
+// let number = 5
+// result = ++number
+// console.log(result);
+
+// Comparison Operators - <, >, =
+// const a = 10
+// const b = 13
+
+// let result = a > b
+// console.log(result);
+
+// result = a < b
+// console.log(result);
+
+// result = a === b
+// console.log(result);
+
+// result = a <= b
+// console.log(result);
+
+// result = a >= b
+// console.log(result);
+
+// result = a != b
+// console.log(result)
+
+// Strict and loose equaility
+// result = 0 === "0"
+// console.log(result)
+
+// result = 0 == "0"
+// console.log(result)
+
+// Logical Operators
+// AND (&&) to check if all opperands are true
+// OR (||) to check if only one opperands is true
+// NOT (!)
+
+// let answer = 0
+// answer = true && true
+// console.log(answer);
+
+// answer = true && false
+// console.log(answer)
+
+
+// // OR 
+// answer = true || true
+// console.log(answer)
+
+// answer = true || false || true || false
+// console.log(answer)
+
+// // NOT
+// answer = !true
+// console.log(answer)
+
+//Assignment Operator 
 
 let number = 5
-result = ++number
-console.log(result);
+console.log(number)
+
+number += 5
+console.log(number)
+
+let word = "hello"
+console.log(word)
+
+word += " muthafucker😉"
+console.log(word)
