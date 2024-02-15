30986674d4c7eac918ce2e2289df0ffafbbd45b2
Callme-Stone <abolajioluwaseyi0@gmail.com>
2024-02-15 15:49:05 +0100

    loop commit

---
+++ logicAndControlFlow/loop/script.js
@@ -0,0 +1,26 @@
+//LOOP
+//while loop
+// let i = 0;
+// while(i < 10) {
+//     console.log(i)
+//     i++
+// }
+//for loop
+// for ([initialzation]; [condtion]; [final - expression]) {
+    
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+for(let i = 1; i<= 8; i++) {
+    console.log(i)
+}
+
+const colors = ['red', 'blue', 'yellow', 'green'];
+
+for(let i = 0; i < colors.length; i++) {
+    console.log(colors[i])
+}
+
