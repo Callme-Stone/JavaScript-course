e313d82e766c366eb8d3db7b431fa346675d0362
Callme-Stone <abolajioluwaseyi0@gmail.com>
2024-02-15 16:03:51 +0100

    loop commit

--- loop/script.js
+++ loop/script.js
@@ -1,18 +1,18 @@
 //LOOP
 //while loop
 // let i = 0;
-// while(i < 10) {
-//     console.log(i)
-//     i++
-// }
+while(i < 10) {
+    console.log(i)
+    i++
+}
 //for loop
 // for ([initialzation]; [condtion]; [final - expression]) {
     
 // }
 
-// for (let i = 0; i < 10; i++) {
-//     console.log(i)
-// }
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
 
 for(let i = 1; i<= 8; i++) {
     console.log(i)
