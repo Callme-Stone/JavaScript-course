99276f379dabca569ed4aea1728a86b6ad8ad09a
Callme-Stone <abolajioluwaseyi0@gmail.com>
2024-02-17 22:19:32 +0100

    myPracticeSpace

---
+++ myPracticeSpace/script.js
@@ -0,0 +1,9 @@
+// If, Else statement
+let kike = 10
+if(kike >= 19) {
+    console.log("You are not my mate")
+} else if(kike >= 10){
+    console.log("Im about to be a teeneager")
+} else {
+    console.log("You dont know me")
+}
