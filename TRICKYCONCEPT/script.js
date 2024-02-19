df01b85d0a690ce33ab49de536c5111b19ca45b8
Callme-Stone <abolajioluwaseyi0@gmail.com>
2024-02-19 21:25:04 +0100

    trickyconcept

--- TRICKYCONCEPT/script.js
+++ TRICKYCONCEPT/script.js
@@ -18,3 +18,15 @@ const localScope =() => {
 }
 const oloye = localScope()
 console.log(oloye)
+
+
+const firstName = "John"
+const lastName = "Doe"
+const education = "Phd.amtomic physics"
+const countryRes = "Nigeria"
+
+const info = () => {
+    return `Mr ${lastName} is the new President of this company. With his experience and ${education} background he will do wonders, but he prefers being called ${firstName}`
+}
+const reply = info()
+console.log(reply)
