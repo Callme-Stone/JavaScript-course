2c263490c4bbfb7d85a542ad2de33838da67bc93
Callme-Stone <abolajioluwaseyi0@gmail.com>
2024-02-20 14:53:18 +0100

    stringsanditsmethods

--- stringsAndItsMethods/script.js
+++ stringsAndItsMethods/script.js
@@ -18,3 +18,15 @@ const sentence = `My name is ${name}, i am ${age} years old and i go to ${school
 
 console.log(sentence)
 
+
+const math = "2 + 2"
+
+const lines = `1
+2
+3
+4
+5`
+console.log(lines)
+
+const numerals = 'Hello Im aliyu and i am from "kogi state"'
+console.log(numerals)
