9c9cd90365bd0c746aff1e7ea5caff86747f533b
Callme-Stone <abolajioluwaseyi0@gmail.com>
2024-02-21 11:59:28 +0100

    arrayindetails

--- arrayInDetails/script.js
+++ arrayInDetails/script.js
@@ -2,3 +2,25 @@ const cohorts = [
     "Oluwaseyi", "David", "Hannah", "Board man", "John doe"
 ]
 console.log(cohorts.length);
+
+const nested = [[1,2], [3,4], [5,6]]
+const newArr = nested[0]
+console.log(newArr)
+
+const object = {
+    name :"John",
+    age : 60,
+}
+
+console.log(object)
+
+const arr = [
+    "Banana",
+    1000,
+    {
+        name : "Bola",
+        age : 30
+    },
+]
+
+console.log(arr)
